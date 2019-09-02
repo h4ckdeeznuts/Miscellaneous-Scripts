b62e3dadc741d78c8c5a982470701c3eded622e8
Kushagra <50336190+h4ckdeeznuts@users.noreply.github.com>
2019-09-02 09:14:25 -0400

    Update Brightness_Adjust.sh

--- Brightness_Adjust.sh
+++ Brightness_Adjust.sh
@@ -1,3 +1,15 @@
+#Brightness_Adjust.sh
+#>Tested on Arch Linux
+#
+#>Run this as sudo, since it writes to the /etc/ dir.
+#
+#>This is a script to adjust the brightness and also has an option to use the Night-Light feature enabled by 'redshift' cli. 
+#
+#>The brightness adjust method only works for intel based laptops, since I personally use a thinkpad with integrated intel HD 4000 graphics.
+#
+#>This script can be modified to be used with any other systems, for universal compatibility which will be added in the future and is
+#my initial attempt at bash scripting.
+
 #!/bin/bash
 
 printf "This is a script for adjusting the screen Brightness"
